06e464069f7be012d0033de6b897da2cc0d34297
Sijan Malla <44745915+season101@users.noreply.github.com>
2021-09-29 17:15:55 +0000

    Need to find help from Wikipedia and make some change today

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 # To write gcd(a,b) as a linear combination of a and b using integer combination i.e au + bv = gcd(a,b)=1
 
+"""
+Find Help in this location
+https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+"""
 def extendedEuclideanAlgorithm(a,b):
-	return u,v;
+	return 1,1
+
+def main():
+	quotients= []
+	quotients.append(1)
+	print(quotients[0])
+	print(extendedEuclideanAlgorithm(73,25))
+
+if __name__== "__main__":
+	main()
