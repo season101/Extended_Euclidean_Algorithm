b84d3925beb1e23e21b16914ef0e6c7714600bc9
Sijan Malla <sijan.malla@dxc.com>
2024-04-20 23:09:10 -0500

    Update todo

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 # To write gcd(a,b) as a linear combination of a and b using integer combination i.e au + bv = gcd(a,b)=1
 
 """
@@ -31,7 +30,7 @@ x(23) + y(25 + 23q1)
 
 Rearranging..
 
-y(25) + (x+q1.y)23, -- (nxt) 
+y(25) + (x+q1.y)23, -- (nxt)
 
 Comparing nxt with general form as well as with (ii)
 We get,
@@ -45,47 +44,46 @@ y = t+y.q2 [q2 is the previous quotient].
 
 
 
-
+Need to expand on it.
 
 
 
 
 """
-def extendedEuclideanAlgorithm(a,b):
-	
-    if(b>a):
-        a,b=b,a
-    
+
+
+def extendedEuclideanAlgorithm(a, b):
+    if b > a:
+        a, b = b, a
 
     quotient = []
-    remainder = a%b
-    while (remainder != 0):
-        quotient.append(-(a//b))
-        a= b
+    remainder = a % b
+    while remainder != 0:
+        quotient.append(-(a // b))
+        a = b
         b = remainder
-        remainder = a%b
+        remainder = a % b
 
-    if(len(quotient) == 0):
-        x=0
-        y=1
-        return x,y
+    if len(quotient) == 0:
+        x = 0
+        y = 1
+        return x, y
     else:
-        x=1
-        y=quotient[len(quotient)-1]
-        i = len(quotient) - 2 
-        while i >=0:
-            t= x
+        x = 1
+        y = quotient[len(quotient) - 1]
+        i = len(quotient) - 2
+        while i >= 0:
+            t = x
             x = y
-            y =t + y*quotient[i]
-            i= i-1
-        
-        return x,y,b
+            y = t + y * quotient[i]
+            i = i - 1
 
+        return x, y, b
 
 
 def main():
-	
-	print(extendedEuclideanAlgorithm(73,25))
+    print(extendedEuclideanAlgorithm(73, 25))
 
-if __name__== "__main__":
-	main()
+
+if __name__ == "__main__":
+    main()
